d4f5325b2db31b0f95cebc61331206d0ac74ab65
drewdroid86 <drewdroid86@gmail.com>
2026-08-02 01:42:41 -0400

    AGY: fix(deps): remove speed-insights and move tsx to production dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { AetherGenesis } from "./components/AetherGenesis";
 
 export default function App() {
   return (
     <div className="w-full h-screen bg-black overflow-hidden">
       <AetherGenesis />
-      <SpeedInsights />
     </div>
   );
 }
